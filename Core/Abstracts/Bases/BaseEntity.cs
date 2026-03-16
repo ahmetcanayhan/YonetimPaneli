1cfbd583dd9ca77b545c943896d195dd1fc1c652
ahmetcanayhan <ahmetayhan38@icloud.com>
2026-03-16 14:37:02 +0300

    Update 02 16.03.2026

---
+++ Core/Abstracts/Bases/BaseEntity.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Core.Abstracts.Bases
+{
+    public class BaseEntity
+    {
+        public int Id { get; set; }
+        public DateTime CreatedAt { get; set; } = DateTime.Now;
+        public bool Active { get; set; } = true;
+        public bool Deleted { get; set; } = false;
+    }
+}
